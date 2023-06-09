cd9b6f75b8258ac8fec834b3c629254e379de3e2
tobytaken <tobiassteinhaeusler@gmail.com>
2023-06-09 16:01:45 +0200

    Add copilot

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -1,6 +1,6 @@
 local plugins = {
 
-  { "elkowar/yuck.vim" , lazy = false },  -- load a plugin at startup
+  { "elkowar/yuck.vim", lazy = false }, -- load a plugin at startup
 
   -- You can use any plugin specification from lazy.nvim
   {
@@ -11,7 +11,7 @@ local plugins = {
     end,
   },
 
-  -- this opts will extend the default opts 
+  -- this opts will extend the default opts
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -29,49 +29,96 @@ local plugins = {
         "c",
         "cmake",
         "c_sharp",
-        "rust"
+        "rust",
       },
     },
   },
 
   -- if you load some function or module within your opt, wrap it with a function
   {
-   "nvim-telescope/telescope.nvim",
-   opts = {
-     defaults = {
-       mappings = {
-         i = {
-           ["<esc>"] = function(...)
-               require("telescope.actions").close(...)
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<esc>"] = function(...)
+              require("telescope.actions").close(...)
             end,
           },
         },
       },
     },
-   },
+  },
 
-   {
-     "folke/which-key.nvim",
-     enabled = false,
-   },
-
-   -- In order to modify the `lspconfig` configuration:
-{
-  "neovim/nvim-lspconfig",
-       dependencies = {
-         "jose-elias-alvarez/null-ls.nvim",
-         config = function()
-       require "custom.configs.null-ls"
-     end,
-   },
-   config = function()
+  {
+    "folke/which-key.nvim",
+    enabled = false,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>",
+          },
+          layout = {
+            position = "bottom", -- | top | left | right
+            ratio = 0.4,
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+          -- accept = "<M-l>",
+            accept = "<Tab>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = "node", -- Node.js version must be > 16.x
+        server_opts_overrides = {},
+      }
+    end,
+  },
+  -- In order to modify the `lspconfig` configuration:
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+    config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
-   end,
-},
-
-
+    end,
+  },
 }
 
 return plugins
-
